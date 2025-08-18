8ef973947aff7b05e65f93d9aeb700915f7aa612
vytautasjc <vytautasjc@users.noreply.github.com>
2025-08-18 22:03:20 +0200

    Add missing links

--- link.sh
+++ link.sh
@@ -9,3 +9,8 @@ SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)"
 ln -sf $SCRIPT_DIR/.aliases $XDG_CONFIG_HOME/.aliases
 ln -sf $SCRIPT_DIR/zsh/.zshenv $HOME/.zshenv
 ln -sf $SCRIPT_DIR/zsh/.zshrc $XDG_CONFIG_HOME/zsh/.zshrc
+ln -sf $SCRIPT_DIR/zsh/.zprofile $XDG_CONFIG_HOME/zsh/.zprofile
+ln -sf $SCRIPT_DIR/zsh/autocomplete.zsh $XDG_CONFIG_HOME/zsh/autocomplete.zsh
+ln -sf $SCRIPT_DIR/zsh/prompt.zsh $XDG_CONFIG_HOME/zsh/prompt.zsh
+
+ln -sf $SCRIPT_DIR/ssh/config $HOME/.ssh/config
