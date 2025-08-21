ab78ce262a62eb10ccfe4f0ee0aed04e0f582d2c
vytautasjc <vytautasjc@users.noreply.github.com>
2025-08-21 21:55:02 +0200

    Updated prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,8 +1,3 @@
-# Based on:
-# Purification
-# by Matthieu Cneude
-# https://github.com/Phantas0s/purification
-
 # prompt:
 # %F => color dict
 # %f => reset color
@@ -11,73 +6,6 @@
 # %n => username
 # %m => shortname host
 # %(?..) => prompt conditional - %(condition.true.false)
-
-# Display git status
-git_prompt_status() {
-  local INDEX STATUS
-
-  INDEX=$(command git status --porcelain -b 2> /dev/null)
-
-  STATUS=""
-
-  if $(echo "$INDEX" | command grep -E '^\?\? ' &> /dev/null); then
-    STATUS="$ZSH_THEME_GIT_PROMPT_UNTRACKED$STATUS"
-  fi
-
-  if $(echo "$INDEX" | grep '^A  ' &> /dev/null); then
-    STATUS="$ZSH_THEME_GIT_PROMPT_ADDED$STATUS"
-  elif $(echo "$INDEX" | grep '^M  ' &> /dev/null); then
-    STATUS="$ZSH_THEME_GIT_PROMPT_ADDED$STATUS"
-  elif $(echo "$INDEX" | grep '^MM ' &> /dev/null); then
-    STATUS="$ZSH_THEME_GIT_PROMPT_ADDED$STATUS"
-  fi
-
-  if $(echo "$INDEX" | grep '^ M ' &> /dev/null); then
-    STATUS="$ZSH_THEME_GIT_PROMPT_MODIFIED$STATUS"
-  elif $(echo "$INDEX" | grep '^AM ' &> /dev/null); then
-    STATUS="$ZSH_THEME_GIT_PROMPT_MODIFIED$STATUS"
-  elif $(echo "$INDEX" | grep '^MM ' &> /dev/null); then
-    STATUS="$ZSH_THEME_GIT_PROMPT_MODIFIED$STATUS"
-  elif $(echo "$INDEX" | grep '^ T ' &> /dev/null); then
-    STATUS="$ZSH_THEME_GIT_PROMPT_MODIFIED$STATUS"
-  fi
-
-  if $(echo "$INDEX" | grep '^R  ' &> /dev/null); then
-    STATUS="$ZSH_THEME_GIT_PROMPT_RENAMED$STATUS"
-  fi
-
-  if $(echo "$INDEX" | grep '^ D ' &> /dev/null); then
-    STATUS="$ZSH_THEME_GIT_PROMPT_DELETED$STATUS"
-  elif $(echo "$INDEX" | grep '^D  ' &> /dev/null); then
-    STATUS="$ZSH_THEME_GIT_PROMPT_DELETED$STATUS"
-  elif $(echo "$INDEX" | grep '^AD ' &> /dev/null); then
-    STATUS="$ZSH_THEME_GIT_PROMPT_DELETED$STATUS"
-  fi
-
-  if $(command git rev-parse --verify refs/stash >/dev/null 2>&1); then
-    STATUS="$ZSH_THEME_GIT_PROMPT_STASHED$STATUS"
-  fi
-
-  if $(echo "$INDEX" | grep '^UU ' &> /dev/null); then
-    STATUS="$ZSH_THEME_GIT_PROMPT_UNMERGED$STATUS"
-  fi
-
-  if $(echo "$INDEX" | grep '^## [^ ]\+ .*ahead' &> /dev/null); then
-    STATUS="$ZSH_THEME_GIT_PROMPT_AHEAD$STATUS"
-  fi
-
-  if $(echo "$INDEX" | grep '^## [^ ]\+ .*behind' &> /dev/null); then
-    STATUS="$ZSH_THEME_GIT_PROMPT_BEHIND$STATUS"
-  fi
-
-  if $(echo "$INDEX" | grep '^## [^ ]\+ .*diverged' &> /dev/null); then
-    STATUS="$ZSH_THEME_GIT_PROMPT_DIVERGED$STATUS"
-  fi
-
-  if [[ ! -z "$STATUS" ]]; then
-    echo "[$STATUS]"
-  fi
-}
 
 
 prompt_git_branch() {
@@ -88,38 +16,73 @@ prompt_git_branch() {
     zstyle ':vcs_info:git:*' formats '%b'
 }
 
-prompt_git_info() {
-    [ ! -z "$vcs_info_msg_0_" ] && echo "$ZSH_THEME_GIT_PROMPT_PREFIX%F{white}$vcs_info_msg_0_%f$ZSH_THEME_GIT_PROMPT_SUFFIX"
+vcs_label() {
+    if [[ -n $vcs_info_msg_0_ ]]; then
+        echo "%F{red}λ%f:${vcs_info_msg_0_}%f"
+    fi
 }
 
-prompt_precmd() {
-    # Pass a line before each prompt
-    print -P ''
+path_label() {
+    echo "%F{blue}%B%~%b%f"
+}
+
+convert_time() {
+    local t=$1
+
+    local d=$((t/1000/60/60/24))
+    local h=$((t/1000/60/60%24))
+    local m=$((t/1000/60%60))
+    local s=$((t/1000%60))
+    local ms=$((t%1000))
+
+    if [[ $d -gt 0 ]]; then
+        echo -n " ${d}d"
+    fi
+
+    if [[ $h -gt 0 ]]; then
+        echo -n " ${h}h"
+    fi
+
+    if [[ $m -gt 0 ]]; then
+        echo -n " ${m}m"
+    fi
+
+    if [[ $s -gt 0 ]]; then
+      echo -n " ${s}s"
+    fi
+
+    if [[ $ms -gt 0 ]]; then
+      echo -n " ${ms}ms"
+    fi
+    
+    echo
+}
+
+precmd() {
+    if [ $timer ]; then
+        now=$(($(print -P %D{%s%6.})/1000))
+        timer_elapsed=$((now - timer))
+        converted_time=$(convert_time "$timer_elapsed")
+
+        medium_gray="%F{240}"
+        print -P ''
+        print -P "${medium_gray}$(date '+%a %b %d, %T'), took${converted_time}%f"
+        print -P ''
+        unset timer
+    fi
+}
+
+preexec() {
+    timer=$(($(print -P %D{%s%6.})/1000))
 }
 
 prompt_setup() {
-    # Display git branch
-
-    autoload -Uz add-zsh-hook
-    add-zsh-hook precmd prompt_precmd
-
-    ZSH_THEME_GIT_PROMPT_PREFIX=" %F{red}λ%f:"
-    ZSH_THEME_GIT_PROMPT_DIRTY=""
-    ZSH_THEME_GIT_PROMPT_CLEAN=""
-
-    ZSH_THEME_GIT_PROMPT_ADDED="%F{green}added%f"
-    ZSH_THEME_GIT_PROMPT_MODIFIED="%F{blue}modified%f"
-    ZSH_THEME_GIT_PROMPT_DELETED="%F{red}deleted%f"
-    ZSH_THEME_GIT_PROMPT_RENAMED="%F{magenta}renamed%f"
-    ZSH_THEME_GIT_PROMPT_UNMERGED="%F{yellow}unmerged%f"
-    ZSH_THEME_GIT_PROMPT_UNTRACKED="%F{white}untracked%f"
-    ZSH_THEME_GIT_PROMPT_STASHED="%B%F{red}stashed%f%b"
-    ZSH_THEME_GIT_PROMPT_BEHIND="%B%F{red}behind%f%b"
-    ZSH_THEME_GIT_PROMPT_AHEAD="%B%F{green}ahead%f%b"
-
     prompt_git_branch
-    RPROMPT='$(prompt_git_info) $(git_prompt_status)'
-    PROMPT=$'%F{blue}%B%~%b%f > '
+
+    NEWLINE=$'\n'
+
+    RPROMPT=""
+    PROMPT='$(path_label) $(vcs_label)${NEWLINE}> '
 }
 
 prompt_setup
