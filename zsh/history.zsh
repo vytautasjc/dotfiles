45e017166387a1ed4299c16b43eda23aaaf3fb6a
vytautasjc <vytautasjc@users.noreply.github.com>
2025-09-09 18:04:45 +0200

    Add alternative history binds

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -27,4 +27,7 @@ zle -N history-beginning-search-backward-end history-search-end
 zle -N history-beginning-search-forward-end history-search-end
 
 bindkey "^[[A" history-beginning-search-backward-end
+bindkey "^[OA" history-beginning-search-backward-end
+
 bindkey "^[[B" history-beginning-search-forward-end
+bindkey "^[OB" history-beginning-search-forward-end
