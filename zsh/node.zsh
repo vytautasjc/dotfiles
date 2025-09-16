1b2d2b7693329289fccd3480977885507495a86d
vytautasjc <vytautasjc@users.noreply.github.com>
2025-09-16 22:57:35 +0200

    Remove symlinking to avoid multi project windows from using incorrect node versions set by other projects

--- zsh/node.zsh
+++ zsh/node.zsh
@@ -1,11 +1,7 @@
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 
 if [ -s "$NVM_DIR/nvm.sh" ]; then
-    export NVM_SYMLINK_CURRENT=true;
-
     . "$NVM_DIR/nvm.sh" --no-use # This loads nvm, without auto-using the default version
-
-    export PATH="$NVM_DIR/current/bin:$PATH"
 
     autoload -U add-zsh-hook
 
