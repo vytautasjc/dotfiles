491b10456c7598e36b9470d6e856543dc0bdd2f6
vytautasjc <vytautasjc@users.noreply.github.com>
2025-12-02 16:59:34 +0100

    Fix zellij init

--- zsh/zellij.zsh
+++ zsh/zellij.zsh
@@ -1,8 +1,17 @@
+export ZELLIJ_AUTO_EXIT=true
+
 # Autostart Zellij
 if [[ -z "$ZELLIJ" ]] && [[ -n "$PS1" ]] && [[ "$TERM" != "dumb" ]]; then
-    if [[ "$ZELLIJ_AUTO_ATTACH" == "true" ]]; then
-        exec zellij attach -c
-    else
-        exec zellij
+    if command -v zellij &> /dev/null; then
+        if [[ "$ZELLIJ_AUTO_ATTACH" == "true" ]]; then
+            zellij attach -c
+        else
+            zellij
+        fi
+
+        # Exit shell when zellij exits
+        if [[ "$ZELLIJ_AUTO_EXIT" == "true" ]]; then
+            exit
+        fi
     fi
 fi
