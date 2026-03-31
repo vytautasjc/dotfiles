0113d7859b2726518c4bd802ba9b0e08eff1df64
vytautasjc <vytautasjc@users.noreply.github.com>
2026-03-31 20:58:13 +0200

    Add missing lib dep

--- nix/users/limadev.nix
+++ nix/users/limadev.nix
@@ -1,6 +1,7 @@
 {
   config,
   pkgs,
+  lib,
   # In case of a VM this should point to a volume that is persisted between VM runs, e.g. a persistent volume
   # This value is passed as env variable from Lima provision scripts
   dataMountPath,
