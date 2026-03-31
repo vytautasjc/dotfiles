3dac9568775a3ff86756954fbf3e48606a611e06
vytautasjc <vytautasjc@users.noreply.github.com>
2026-03-31 10:23:53 +0200

    Add nix helper function

--- zsh/zshrc.d/nix.zsh
+++ zsh/zshrc.d/nix.zsh
@@ -2,3 +2,6 @@ if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
   . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
 fi
 
+function hms() {
+	nix run github:nix-community/home-manager -- switch --flake ".#$(whoami)" -b hm-backup
+}
